c14bb9e2db6c66ca2114d45dc60aa9720da911b4
Marcos Sevilla <marcossevilla14@gmail.com>
2020-11-10 18:04:04 -0600

    added the first view - home page

--- lib/src/core/app.dart
+++ lib/src/core/app.dart
@@ -1,18 +1,13 @@
 import 'package:flutter/material.dart';
 
+import '../number_trivia/presentation/views/home_page.dart';
+
 class CleanTriviaApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Clean Trivia App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }
